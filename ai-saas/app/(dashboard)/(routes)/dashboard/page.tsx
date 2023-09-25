acdb1fe69411e0d2b6797be1f6945f24f481cf98
hjasaiwal114 <68457116+hjasaiwal114@users.noreply.github.com>
2023-09-25 11:52:36 +0530

    error finding and done

--- ai-saas/app/(dashboard)/(routes)/dashboard/page.tsx
+++ ai-saas/app/(dashboard)/(routes)/dashboard/page.tsx
@@ -1,7 +1,35 @@
+"use client";
+
+import { MessageSquare } from "lucide-react";
+
+const tools = [
+    {
+        label: "Conversation",
+        icon: MessageSquare,
+        color: 'text-violet-500',
+        bgColor: "bg-violet-500/10",
+        href: "/conversation"
+
+    }
+]
+
 const DashboardPage = () => {
     return (
         <div>
-           <p>Dashboard Page (protected)</p>
+            <div className="mb-8 space-y-4">
+                <h2 className="text-2xl md:text-4xl font-bold">
+                    Explore the power of the ai
+                </h2>
+                <p className="text-muted-foreground font-light text-sm
+                ms: text-lg text-center">
+                    Chat with the smarter Ai -Experience the power of AI
+                </p>
+            </div>
+            <div className="px-4 md:px-20 lg:px-32 space-y-4">
+                { tools.map((tool) => (
+                    
+                ))}
+            </div>
         </div>  
     )
 }
